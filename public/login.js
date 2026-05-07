b3b83a581e8cacfbf4f890e92e8a4210451df469
LeonardoBBeata <leonardo.beata@aluno.cps.sp.gov.br>
2026-05-07 07:49:55 -0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,82 +1,32 @@
-const API = "https://formulavest-2.onrender.com";
-
-const registerBox =
-document.getElementById('register-box');
-
-document.getElementById('toggle-register')
-.onclick = ()=>{
-
-    registerBox.classList.toggle('hidden');
-};
-
-document.getElementById('login-btn')
-.onclick = async ()=>{
-
-    const username =
-    document.getElementById('login-username').value;
-
-    const senha =
-    document.getElementById('login-senha').value;
-
-    const res = await fetch(`${API}/login`,{
-        method:'POST',
-
-        headers:{
-            'Content-Type':'application/json'
-        },
-
-        credentials:'include',
-
-        body:JSON.stringify({
-            username,
-            senha
-        })
-    });
-
-    const data = await res.json();
-
-    if(data.ok){
-
-        window.location.href = '/';
-
-    }else{
-
-        alert(data.error);
-    }
-};
-
-document.getElementById('register-btn')
-.onclick = async ()=>{
-
-    const username =
-    document.getElementById('reg-username').value;
-
-    const senha =
-    document.getElementById('reg-senha').value;
-
-    const res = await fetch(`${API}/register`,{
-        method:'POST',
-
-        headers:{
-            'Content-Type':'application/json'
-        },
-
-        credentials:'include',
-
-        body:JSON.stringify({
-            username,
-            senha
-        })
-    });
-
-    const data = await res.json();
-
-    if(data.ok){
-
-        alert('Conta criada!');
-
-    }else{
-
-        alert(data.error);
-    }
-};
+const API = 'https://SEU-BACKEND.onrender.com';
+async function login(){
+const username = document
+.getElementById('login-username')
+.value;
+const senha = document
+.getElementById('login-senha')
+.value;
+const res = await fetch(`${API}/login`,{
+method:'POST',
+headers:{
+'Content-Type':'application/json'
+},
+body:JSON.stringify({
+username,
+10
+senha
+})
+});
+const data = await res.json();
+if(data.ok){
+localStorage.setItem(
+'token',
+data.token
+);
+localStorage.setItem(
+'user',
+JSON.stringify(data.user)
+);
+window.location.href = '/';
+}
+}
