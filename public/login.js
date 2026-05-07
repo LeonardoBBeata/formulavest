dee153eea6f887ef0f7d3014a23bc807aae34ec8
LeonardoBBeata <leonardo.beata@aluno.cps.sp.gov.br>
2026-05-07 07:54:32 -0300

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,32 +1,13 @@
-const API = 'https://SEU-BACKEND.onrender.com';
-async function login(){
-const username = document
-.getElementById('login-username')
-.value;
-const senha = document
-.getElementById('login-senha')
-.value;
-const res = await fetch(`${API}/login`,{
-method:'POST',
-headers:{
-'Content-Type':'application/json'
-},
-body:JSON.stringify({
-username,
+constAPI='https://SEU-BACKEND.onrender.com';
+// LOGIN
+document.getElementById('login-btn')
+.onclick=async()=>{constusername=document.getElementById('login-username').value;constsenha=document.getElementById('login-senha').value;
 10
-senha
-})
-});
-const data = await res.json();
-if(data.ok){
-localStorage.setItem(
-'token',
-data.token
-);
-localStorage.setItem(
-'user',
-JSON.stringify(data.user)
-);
-window.location.href = '/';
-}
-}
+constres=awaitfetch(`${API}/login`,{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({username,senha})});constdata=awaitres.json();if(data.ok){localStorage.setItem('token',data.token);localStorage.setItem('user',JSON.stringify(data.user));window.location.href='/';}else{alert(data.error);}
+};
+// REGISTER
+document.getElementById('register-btn')
+.onclick=async()=>{constusername=document.getElementById('reg-username').value;constsenha=document.getElementById('reg-senha').value;constres=awaitfetch(`${API}/register`,{
+11
+method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({username,senha})});constdata=awaitres.json();if(data.ok){alert('Conta criada');}else{alert(data.error);}
+};
