354689299b0f0f3ad195c88b7c954aba4e40d427
LeonardoBBeata <leonardo.beata@aluno.cps.sp.gov.br>
2026-05-11 12:46:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -32,25 +32,23 @@ const db = new Pool({
 // ======================
 // EMAIL
 // ======================
-
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
-    secure: true,
+    secure: process.env.SMTP_SECURE === 'true',
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
     }
 });
 
-transporter.verify((error, success) => {
+transporter.verify((error) => {
     if (error) {
         console.error('ERRO SMTP:', error);
     } else {
         console.log('SMTP conectado com sucesso!');
     }
 });
-
 
 
 // ======================
