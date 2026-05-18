aaf54d5325879352a31120244a79271efcc53b75
LeonardoBBeata <leonardo.beata@aluno.cps.sp.gov.br>
2026-05-18 10:56:19 -0300

    Update brevo.js

--- config/brevo.js
+++ config/brevo.js
@@ -1,1 +1,10 @@
+const SibApiV3Sdk = require('@getbrevo/brevo');
 
+const brevo = new SibApiV3Sdk.TransactionalEmailsApi();
+
+brevo.setApiKey(
+  SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
+
+module.exports = brevo;
