54b841e5968cb1e633d5b6827695ea472b8c2a07
LeonardoBBeata <leonardo.beata@aluno.cps.sp.gov.br>
2026-05-18 10:56:36 -0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -1,1 +1,21 @@
+const brevo = require('../config/brevo');
 
+async function enviarEmail(
+  para,
+  assunto,
+  texto,
+  html = null
+) {
+  await brevo.sendTransacEmail({
+    sender: {
+      name: "FórmulaVest",
+      email: process.env.EMAIL_FROM
+    },
+    to: [{ email: para }],
+    subject: assunto,
+    textContent: texto,
+    htmlContent: html || `<p>${texto}</p>`
+  });
+}
+
+module.exports = enviarEmail;
