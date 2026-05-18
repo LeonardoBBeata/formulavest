7e5a3248ec6de9653fd848a0e1772ca3b23ceac9
LeonardoBBeata <leonardo.beata@aluno.cps.sp.gov.br>
2026-05-18 10:57:02 -0300

    Update roles.js

--- middleware/roles.js
+++ middleware/roles.js
@@ -1,1 +1,23 @@
+function permitir(...roles) {
+  return (req, res, next) => {
 
+    if (
+      req.user.role ===
+      "formulavest_master"
+    ) {
+      return next();
+    }
+
+    if (
+      !roles.includes(req.user.role)
+    ) {
+      return res.status(403).json({
+        error: "Sem permissão"
+      });
+    }
+
+    next();
+  };
+}
+
+module.exports = permitir;
